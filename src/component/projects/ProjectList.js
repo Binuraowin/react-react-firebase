1eae74a29bdc135d5ec60e21b1e32011db6c690b
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-12-17 13:46:12 +0530

    project summary

--- src/component/projects/ProjectList.js
+++ src/component/projects/ProjectList.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import Notification from "../dashboard/Notifications";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () =>{
     return (
         <div className={"project-list section"}>
-            <div className={"card 2-depth-0 project-summary"}>
-                <div className={"card-content grey-text text-darkrn-3"}>
-                    <span className={"card-title"}>Title</span>
-                    <p>Poested by binura</p>
-                    <p className={"grey-text"}> Date</p>
-                </div>
-
-            </div>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div>
     )
 }
---
+++ src/component/projects/ProjectSummary.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const ProjectSummary = () =>{
+    return (
+        <div className={"project-list section"}>
+            <div className={"card 2-depth-0 project-summary"}>
+                <div className={"card-content grey-text text-darkrn-3"}>
+                    <span className={"card-title"}>Title</span>
+                    <p>Poested by binura</p>
+                    <p className={"grey-text"}> Date</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default ProjectSummary
